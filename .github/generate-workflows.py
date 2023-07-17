747dbe4b7123a5ae1fe97ddc044bbccbcfba0d5a
Oliver Kopp <kopp.dev@gmail.com>
2023-07-18 01:12:56 +0200

    Fix Dockerfile.iot

--- .github/generate-workflows.py
+++ .github/generate-workflows.py
@@ -84,6 +84,30 @@ for documentclass in documentclasses:
       - run: npm install
       - run: mkdir /tmp/out
 ''')
+              ymlmiktex = open("workflows/miktex-check-{}.yml".format(dashedPart), "w+")
+              ymlmiktex.write("name: MiKTeX {}\n".format(dashedPart))
+              ymlmiktex.write("on: [push]\n")
+              ymlmiktex.write("concurrency:\n")
+              ymlmiktex.write("  group: miktex-{}\n".format(dashedPart))
+              ymlmiktex.write("  cancel-in-progress: true\n")
+              ymlmiktex.write("jobs:\n")
+              ymlmiktex.write("  miktex:\n")
+              ymlmiktex.write('''    runs-on: ubuntu-22.04
+    steps:
+      - name: Install MikTeX
+        run: |
+          set -e
+          sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/miktex.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+          echo "deb [arch=amd64 signed-by=/usr/share/keyrings/miktex.gpg] http://miktex.org/download/ubuntu jammy universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+          sudo apt-get update -y
+          sudo apt-get install -y --no-install-recommends miktex
+          sudo miktexsetup finish
+          sudo initexmf --admin --set-config-value=[MPM]AutoInstall=1
+          sudo mpm --admin --update-db
+          sudo mpm --admin --update
+      - name: Checkout repository
+        uses: actions/checkout@v3
+''')
               for howtotext in howtotexts:
                 for language in languages:
                   for font in fonts:
@@ -94,35 +118,37 @@ for documentclass in documentclasses:
                         for tweakouterquote in tweakouterquotes:
                           for todo in todos:
                               variantName = "{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}".format(documentclass, latexcompiler, bibtextool, texlive, language, font, listing, enquote, tweakouterquote, todo, example, howtotext)
-                              yml.write("      - run: mkdir {}\n".format(variantName))
-                              yml.write("      - name: Create {}\n".format(variantName))
-                              yml.write('''        run: |
+                              yml_content = "      - run: mkdir {}\n".format(variantName)
+                              yml_content += "      - name: Create {}\n".format(variantName)
+                              yml_content += '''        run: |
           npx yo $GITHUB_WORKSPACE\\
-''')
-                              yml.write("           --documentclass=%s\\\n" % documentclass)
-                              if (documentclass == 'ieee'):
-                                yml.write("           --ieeevariant=%s\\\n" % ieeevariant)
-                              if (documentclass == 'acmart'):
-                                yml.write("           --acmformat=%s\\\n" % acmformats[0])
-                                yml.write("           --acmreview=%s\\\n" % acmreviews[0])
-                              yml.write("           --papersize=%s\\\n" % papersize)
-                              yml.write("           --latexcompiler=%s\\\n" % latexcompiler)
-                              yml.write("           --bibtextool=%s\\\n" % bibtextool)
-                              yml.write("           --overleaf=false\\\n")
-                              yml.write("           --texlive=%s\\\n" % texlive)
-                              yml.write("           --docker=iot\\\n")
-                              yml.write("           --lang=%s\\\n" % language)
-                              yml.write("           --font=%s\\\n" % font)
-                              yml.write("           --listings=%s\\\n" % listing)
-                              yml.write("           --enquotes=%s\\\n" % enquote)
-                              yml.write("           --tweakouterquote=%s\\\n" % tweakouterquote)
-                              yml.write("           --todo=%s\\\n" % todo)
-                              yml.write("           --examples=%s\\\n" % example)
-                              yml.write("           --howtotext=%s\n" % howtotext)
-                              yml.write('''        env:
+'''
+                              yml_content += "           --documentclass=%s\\\n" % documentclass
+                              if documentclass == 'ieee':
+                                  yml_content += "           --ieeevariant=%s\\\n" % ieeevariant
+                              if documentclass == 'acmart':
+                                  yml_content += "           --acmformat=%s\\\n" % acmformats[0]
+                                  yml_content += "           --acmreview=%s\\\n" % acmreviews[0]
+                              yml_content += "           --papersize=%s\\\n" % papersize
+                              yml_content += "           --latexcompiler=%s\\\n" % latexcompiler
+                              yml_content += "           --bibtextool=%s\\\n" % bibtextool
+                              yml_content += "           --overleaf=false\\\n"
+                              yml_content += "           --texlive=%s\\\n" % texlive
+                              yml_content += "           --docker=iot\\\n"
+                              yml_content += "           --lang=%s\\\n" % language
+                              yml_content += "           --font=%s\\\n" % font
+                              yml_content += "           --listings=%s\\\n" % listing
+                              yml_content += "           --enquotes=%s\\\n" % enquote
+                              yml_content += "           --tweakouterquote=%s\\\n" % tweakouterquote
+                              yml_content += "           --todo=%s\\\n" % todo
+                              yml_content += "           --examples=%s\\\n" % example
+                              yml_content += "           --howtotext=%s\n" % howtotext
+                              yml_content += '''        env:
           yeoman_test: true
-''')
-                              yml.write("        working-directory: '${{{{ github.workspace }}}}/{}'\n".format(variantName))
+'''
+                              yml_content += "        working-directory: '${{{{ github.workspace }}}}/{}'\n".format(variantName)
+                              yml.write(yml_content)
+                              ymlmiktex.write(yml_content)
                               yml.write('''      - name: Build docker image
         uses: docker/build-push-action@v3
         with:
@@ -131,13 +157,17 @@ for documentclass in documentclasses:
                               yml.write("          tags: localhost:5000/name/app:{}\n".format(variantName))
                               yml.write("          context: '${{{{ github.workspace }}}}/{}'\n".format(variantName))
                               yml.write("      - name: latexmk {}\n".format(variantName))
+                              ymlmiktex.write("      - name: latexmk {}\n".format(variantName))
                               filename = "paper.tex" if ((documentclass == 'acmart') or (documentclass == 'lncs') or (documentclass == 'ieee')) else "main.tex"
                               command = "latexmk {}".format(filename) if (docker != 'reitzig') else "work latexmk {}".format(filename)
-                              yml.write("        run: docker run -v $(pwd):/work/src -v /tmp/out:/work/out localhost:5000/name/app:{} {}\n".format(variantName, command))
+                              yml.write("        run: docker run -v $(pwd):/workdir -v /tmp/out:/work/out localhost:5000/name/app:{} {}\n".format(variantName, command))
+                              ymlmiktex.write("        run: {}\n".format(command))
                               yml.write("        working-directory: '${{{{ github.workspace }}}}/{}'\n".format(variantName))
+                              ymlmiktex.write("        working-directory: '${{{{ github.workspace }}}}/{}'\n".format(variantName))
               yml.write('''      - uses: actions/upload-artifact@v2
         with:
           name: pdfs
           path: /tmp/out
 ''')
               yml.close()
+              ymlmiktex.close()
