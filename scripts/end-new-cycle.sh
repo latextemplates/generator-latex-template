19d24218fb09e05c89cddbaa719cda58f18dff78
Oliver Kopp <kopp.dev@gmail.com>
2025-02-14 08:22:04 +0100

    Update end-new-cycle

--- scripts/end-new-cycle.sh
+++ scripts/end-new-cycle.sh
@@ -4,17 +4,31 @@ set -e
 # Current branch of generator-latex-template needs to be "main" and point to the latest release
 # Each template needs to be on the branch "update-ltg"
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$current_branch" != "main" ]]; then
+    echo "Error: Current branch is '$current_branch'. Expected 'main'."
+    exit 1
+fi
+
 cd ..
 
 for template in *-enhanced uni-stuttgart-dissertation-template; do
   echo "$template"
   cd $template
-  git pull --no-edit
+
+  # ensure update-ltg to be in line with origin/update-ltg
+  git fetch
+  git checkout --force origin/update-ltg
+  git branch -D update-ltg || true
+  git checkout update-ltg
+
   echo "Updating generator-latex-template..."
   cd generator-latex-template
   git checkout --force main
-  git pull
+  git fetch
+  git reset --hard origin/main
   cd ..
+
   echo "Adding generator-latex-template..."
   git add generator-latex-template
   git commit -m"Update LTG" || true
